63a54ab3400c17d0917b9c3a7737c28aae509fbb
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-29 19:24:27 +0000

    MB update

--- ingredients-image-detection/ingredients-prediction-with-preprocessing.py
+++ ingredients-image-detection/ingredients-prediction-with-preprocessing.py
@@ -4,11 +4,24 @@
 # use: python3 (y or n for using preprocessing) file_path
 
 def preprocess_background(input_img):
-    #== Libraries =======================================================================
+    '''
+    This function preprocesses an image based on cv2.
+    It tries to remove the background of an image.
+    The intention is to make then prediction easier for the model.
+    
+    Input:
+    - Image object
+    
+    Output:
+    - Preprocessed image
+    
+    Source: https://stackoverflow.com/questions/29313667/how-do-i-remove-the-background-from-this-kind-of-image
+    '''
+
     import numpy as np
     import cv2
     
-    #== Parameters =======================================================================
+    # Parameters
     BLUR = 21
     CANNY_THRESH_1 = 10
     CANNY_THRESH_2 = 200
@@ -20,18 +33,15 @@ def preprocess_background(input_img):
     input_shape = (100, 100, 3)
 
 
-    #== Processing =======================================================================
-
-    #-- Convert image -----------------------------------------------------------------------
+    # Convert image 
     img = (input_img*255).astype(np.uint8)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    #-- Edge detection -------------------------------------------------------------------
+    # Edge detection 
     edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
     edges = cv2.dilate(edges, None)
     edges = cv2.erode(edges, None)
 
-    #-- Find contours in edges, sort by area ---------------------------------------------
     contour_info = []
     contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
@@ -45,18 +55,18 @@ def preprocess_background(input_img):
     contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
     max_contour = contour_info[0]
 
-    #-- Create empty mask, draw filled polygon on it corresponding to largest contour ----
+    # Create empty mask, draw filled polygon on it corresponding to largest contour
     # Mask is black, polygon is white
     mask = np.zeros(edges.shape)
     cv2.fillConvexPoly(mask, max_contour[0], (255))
 
-    #-- Smooth mask, then blur it --------------------------------------------------------
+    # Smooth mask, then blur it 
     mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
     mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
     mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
-    mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
-
-    #-- Blend masked img into MASK_COLOR background --------------------------------------
+    mask_stack = np.dstack([mask]*3)
+    
+    # Blend masked img into MASK_COLOR background 
     mask_stack  = mask_stack.astype('float32') / 255.0          # Use float matrices, 
     img         = img.astype('float32') / 255.0                 #  for easy blending
 
@@ -69,10 +79,14 @@ def preprocess_background(input_img):
     return image
 
 def predict_image(label_file='/home/miber/creative-cooking/labels.txt', model_path='/home/miber/creative-cooking/ingredients_training_output_files/ingredients_detection_model/model.h5'):
-    #== load model =======================================================================
+    '''
+    Making a single image prediction. Image path and model path are required.
+    '''
+    
+    # load model
     model = load_model(model_path)
 
-    #== load image =======================================================================
+    # load image 
     image_size = (100, 100)
     input_shape = (1, 100, 100, 3) 
 
@@ -85,14 +99,14 @@ def predict_image(label_file='/home/miber/creative-cooking/labels.txt', model_pa
     image = cv2.resize(image, image_size)
     image = np.reshape(image, input_shape)
 
-    #== make prediction =======================================================================
+    # make prediction
     learning_rate = 0.1
     optimizer = Adadelta(lr=learning_rate)
     model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
     prediction = model.predict(image)
 
-    #== format prediction and yield top 5 results with prob ===================================
+    # format prediction and yield top 5 results with prob 
     label_file = label_file
 
     with open(label_file, "r") as f:
@@ -104,7 +118,7 @@ def predict_image(label_file='/home/miber/creative-cooking/labels.txt', model_pa
     
 if __name__ == '__main__':
     
-    #== Libraries =======================================================================
+    # Libraries
     import os
     # No messages 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -122,5 +136,5 @@ if __name__ == '__main__':
     import cv2
     import numpy as np
     
-    #== Predict image =======================================================================
+    # Predict image
     predict_image()
